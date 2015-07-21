42631e2266e42d55e68ee4ce9c80e24a71746e9b
Massimo Milan <massimo.milan@gmail.com>
2015-07-21 13:11:20 +0200

    first working version

--- Storing 7/AppDelegate.swift
+++ Storing 7/AppDelegate.swift
@@ -13,7 +13,12 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
 
+    func application(app: UIApplication, openURL url: NSURL, sourceApplication: String? , annotation: AnyObject) -> Bool {
+        print("app: \(app), url: \(url), caller: \(sourceApplication)")
+        return true
+    }
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
--- Storing 7/ViewController.swift
+++ Storing 7/ViewController.swift
@@ -7,19 +7,126 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
 
+    @IBOutlet weak var label: UILabel!
+    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView:UIView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        var captureSession:AVCaptureSession?
 
+        
+        let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+
+        let input: AnyObject! = try! AVCaptureDeviceInput.init(device: captureDevice) as AVCaptureDeviceInput
+        
+        captureSession = AVCaptureSession()
+        captureSession?.addInput(input as! AVCaptureInput)
+        
+        let captureMetadataOutput = AVCaptureMetadataOutput()
+        captureSession?.addOutput(captureMetadataOutput)
+        
+        captureMetadataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+        captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+
+
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        view.layer.addSublayer(videoPreviewLayer!)
+
+        captureSession?.startRunning()
+        
+        view.bringSubviewToFront(label)
+        
+        qrCodeFrameView = UIView()
+        qrCodeFrameView?.layer.borderColor = UIColor.greenColor().CGColor
+        qrCodeFrameView?.layer.borderWidth = 2
+        view.addSubview(qrCodeFrameView!)
+        view.bringSubviewToFront(qrCodeFrameView!)
+
+
+        let installationId = NSUserDefaults.standardUserDefaults().stringForKey("installationId")
+        if let id = installationId {
+            label.text = id
+        }
+        else {
+            let url = NSURL(string: "https://authidtest.asp.lifeware.ch/v0App/newInstallation?deviceName=iphone6")
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+                let id = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                NSUserDefaults.standardUserDefaults().setObject(id, forKey: "installationId")
+                NSUserDefaults.standardUserDefaults().synchronize()
+                self.label.text = id as? String
+            }
+            
+            task!.resume()
+        }
+        
+    }
+    
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+        if metadataObjects == nil || metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRectZero
+            return
+        }
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        if metadataObj.type == AVMetadataObjectTypeQRCode {
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObjectForMetadataObject(metadataObj as AVMetadataMachineReadableCodeObject) as!AVMetadataMachineReadableCodeObject
+            qrCodeFrameView?.frame = barCodeObject.bounds;
+            if metadataObj.stringValue != nil {
+                label.text = metadataObj.stringValue
+            }
+        }
+    }
+    
+    func mapOriention(orientation: UIInterfaceOrientation) -> AVCaptureVideoOrientation {
+        switch (orientation) {
+            case .Portrait: return .Portrait;
+            case .PortraitUpsideDown: return .PortraitUpsideDown;
+            case .LandscapeLeft: return .LandscapeLeft;
+            case .LandscapeRight: return .LandscapeRight;
+            default: return .Portrait;
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        videoPreviewLayer!.connection.videoOrientation = self.mapOriention(UIApplication.sharedApplication().statusBarOrientation)
+        videoPreviewLayer?.frame = view.layer.bounds
+    }
+    
+    @IBAction func tapped(sender: UITapGestureRecognizer) {
+        let label = sender.view as! UILabel
+        let token = NSURL(string: label.text!)?.lastPathComponent
+        let installationId = NSUserDefaults.standardUserDefaults().stringForKey("installationId")
+
+        let request = "https://authidtest.asp.lifeware.ch/v0App/authenticateBarcode?barcodeToken=\(token!)&installationId=\(installationId!)"
+        let url = NSURL(string: request)
+        
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+            let response = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print("response \(response)")
+        }
+        
+        task!.resume()
+
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func longpress(sender: UILongPressGestureRecognizer) {
+        NSUserDefaults.standardUserDefaults().removeObjectForKey("installationId")
+        print("long press")
+    }
 
 }
 
